be887966fc69faf8fa42e97b5f1d7aa7cb579caf
Charlie Hills <github.cb25@tmx.email>
2022-07-07 23:11:48 -0500

    Add a TOC generator
    This reads a directory and generates a large `<ul>` tag containing links
    to every chapter.
    
    It's not intended to generate a finished product. There's still going to
    be some manual edits. But this takes care of the huge majority of the
    editing.

---
+++ lib/Toc.php
@@ -0,0 +1,67 @@
+<?php namespace App\lib;
+
+use domDocument;
+
+class Toc
+{
+    /** @var domDocument */
+    private domDocument $output;
+
+    /**
+     * Initialize the output Document
+     * @param string $template
+     */
+    public function initializeOutput(string $template = 'template/toc.xhtml')
+    {
+        $this->output = new domDocument('1.0', 'UTF-8');
+        $this->output->load($template);
+    }
+
+    /**
+     * Add a chapter to the output document
+     * @param string $fileName
+     * @param string $inputDir
+     */
+    public function addChapter(string $fileName, string $inputDir)
+    {
+        echo "Processing file $fileName\n";
+
+        // Add file to document
+        $this->addToToc($fileName, $inputDir);
+    }
+
+    /**
+     * @param string $fileName
+     * @param string $inputDir
+     */
+    private function addToToc(string $fileName, string $inputDir)
+    {
+        // Extract title
+        $recipe = new domDocument('1.0', 'UTF-8');
+        $recipe->load($inputDir . '/' . $fileName);
+        $title = $recipe->getElementsByTagName('title');
+        $title = $title->item(0);
+
+        // Now add li/a tags
+        $toc = $this->output->getElementsByTagName('ul');
+        $toc = $toc->item(0);
+
+        $item = $this->output->createElement('li');
+        $toc->appendChild($item);
+
+        $anchor = $this->output->createElement('a');
+        $anchor->setAttribute('href', 'xhtml/' . $fileName);
+        $anchor->nodeValue = $title->nodeValue;
+        $item->appendChild($anchor);
+    }
+
+    /**
+     * Finalize processing and return output Document
+     * @return string
+     */
+    public function finalize(): string
+    {
+        $this->output->formatOutput = true;
+        return $this->output->saveXML();
+    }
+}
---
+++ toc.php
@@ -0,0 +1,34 @@
+<?php namespace App;
+
+// Get command line options
+$opts = "i:o:";
+$options = getopt($opts);
+
+// Load transformation engine
+require_once('lib/Toc.php');
+$toc = new lib\Toc();
+$toc->initializeOutput();
+
+// Process files
+processDirectory($toc, $options['i']);
+
+// Save output package
+file_put_contents($options['o'], $toc->finalize());
+
+function processDirectory(lib\Toc $toc, string $inputDir)
+{
+    // Get files
+    $files = scandir($inputDir);
+
+    // Process files
+    foreach ($files as $file) {
+
+        $inputFileName = $inputDir . '/' . $file;
+        if (substr($inputFileName, -6) !== '.xhtml') {
+            continue;
+        }
+
+        $toc->addChapter($file, $inputDir);
+
+    }
+}
