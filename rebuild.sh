9f8709ed82dd6965598db321ae394588723383b6
Charlie Hills <chills@apple.com>
2022-10-29 15:15:52 -0500

    Add support for soft-coding the book via PROJECT_NAME env var
    This still isn't a general purpose script. It still requires tweaking
    here and there: not only for the project being worked on but for what
    stage we are in the process.

--- rebuild.sh
+++ rebuild.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
+
+if [[ -z "${PROJECT_NAME}" ]]; then
+  echo "Must define PROJECT_NAME environment variable"
+  echo "This should match the github repo name of the book (e.g., mbk-cfo)"
+  exit 1
+fi
+
 echo "Installing base"
-rm -rf ../mbk-cfo/target/
-SOURCE=../mbk-cfo/source/OEBPS TARGET=../mbk-cfo/target/OEBPS make install
+rm -rf ../$PROJECT_NAME/target/
+SOURCE=../$PROJECT_NAME/source/OEBPS TARGET=../mbk-cfo/target/OEBPS make install
 sleep 1
 
 echo "Running XHTML transformations"
-SOURCE=../mbk-cfo/source/OEBPS TARGET=../mbk-cfo/target/OEBPS make transform
+SOURCE=../$PROJECT_NAME/source/OEBPS TARGET=../mbk-cfo/target/OEBPS make transform
 sleep 1
 
 #echo "Building TOC"
-#TARGET=../mbk-cfo/target/OEBPS make toc
+#TARGET=../$PROJECT_NAME/target/OEBPS make toc
 #sleep 1
 
 echo "Building OPF"
-TARGET=../mbk-cfo/target/OEBPS make package
+TARGET=../$PROJECT_NAME/target/OEBPS make package
 sleep 1
 
 echo "Generating QR Code links"
-SOURCE=../mbk-cfo/target/OEBPS/xhtml make qr-linker
+SOURCE=../$PROJECT_NAME/target/OEBPS/xhtml make qr-linker
