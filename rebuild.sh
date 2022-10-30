febfcdcf2155a1cff1b3c220d4cb1a03363cab23
Charlie Hills <chills@apple.com>
2022-10-30 13:39:15 -0500

    Update rebuild.sh
    * Found a couple straggling hard-coded references to mbk-cfo
    * Added the new toc + toc2 + ncx combo
    * Commented out qr-linker
    
    I'll figure out a better way to do this some other time. Though, if we
    start with epub from the get-go with the next book (and we will) then
    it's likely we won't use this ever again.

--- rebuild.sh
+++ rebuild.sh
@@ -8,20 +8,22 @@ fi
 
 echo "Installing base"
 rm -rf ../$PROJECT_NAME/target/
-SOURCE=../$PROJECT_NAME/source/OEBPS TARGET=../mbk-cfo/target/OEBPS make install
+SOURCE=../$PROJECT_NAME/source/OEBPS TARGET=../$PROJECT_NAME/target/OEBPS make install
 sleep 1
 
 echo "Running XHTML transformations"
-SOURCE=../$PROJECT_NAME/source/OEBPS TARGET=../mbk-cfo/target/OEBPS make transform
+SOURCE=../$PROJECT_NAME/source/OEBPS TARGET=../$PROJECT_NAME/target/OEBPS make transform
 sleep 1
 
-#echo "Building TOC"
-#TARGET=../$PROJECT_NAME/target/OEBPS make toc
-#sleep 1
+echo "Building TOC"
+TARGET=../$PROJECT_NAME/target/OEBPS make toc
+SOURCE=../$PROJECT_NAME/source/OEBPS TARGET=../$PROJECT_NAME/target/OEBPS make toc2
+SOURCE=../$PROJECT_NAME/source/OEBPS/0004_toc.xhtml TARGET=../$PROJECT_NAME/build/OEBPS/xhtml/toc.ncx make ncx
+sleep 1
 
 echo "Building OPF"
 TARGET=../$PROJECT_NAME/target/OEBPS make package
 sleep 1
 
-echo "Generating QR Code links"
-SOURCE=../$PROJECT_NAME/target/OEBPS/xhtml make qr-linker
+#echo "Generating QR Code links"
+#SOURCE=../$PROJECT_NAME/target/OEBPS/xhtml make qr-linker
